01e21ce9563a9650ddb294395d529adf8bb0890a
keshavdulal <Keshav.dulal@gmail.com>
2021-12-12 09:28:16 +0545

    feat(CoRoutines): fetch 4 more images - wip

--- app/src/main/java/com/example/aishwaryashrestha_keshavdulal_mapd711_assignment2/BookingActivity.kt
+++ app/src/main/java/com/example/aishwaryashrestha_keshavdulal_mapd711_assignment2/BookingActivity.kt
@@ -9,11 +9,14 @@
 package com.example.aishwaryashrestha_keshavdulal_mapd711_assignment2
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -24,6 +27,8 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.aishwaryashrestha_keshavdulal_mapd711_assignment2.databinding.ActivityMain2Binding
 import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class BookingActivity : AppCompatActivity() {
@@ -120,27 +125,83 @@ class BookingActivity : AppCompatActivity() {
         }
 
         // BUGGY CODE BLOCK - BEGIN
+        val textViewStatus = findViewById<TextView>(R.id.imageFetchStatus)
+        textViewStatus.text="Image Fetch Status: Unknown"
         // fetch cruise images from url using coroutines and store it on imageView
-//        GlobalScope.launch(Dispatchers.IO) {
-//            // N/w activity is ran on Background Thread
-//            // Error seems to be happening on this thread
-//
-//            val imageUrl =
+        GlobalScope.launch(Dispatchers.IO) {
+            // N/w activity is ran on Background Thread
+
+            // bahamas
+            val imageUrl =
+                        "https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80"
+            val url = URL(imageUrl)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+
+            val inputStream = connection.inputStream
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+
+            // caribbean
+//            val imageUrl2 =
 //                "https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80"
-//            val url = URL(imageUrl)
-//            val connection = url.openConnection() as HttpURLConnection
-//            connection.doInput = true
-//            connection.connect()
+//            val url2 = URL(imageUrl2)
+//            val connection2 = url2.openConnection() as HttpURLConnection
+//            connection2.doInput = true
+//            connection2.connect()
+//            val inputStream2 = connection2.inputStream
+//            val bitmap2 = BitmapFactory.decodeStream(inputStream2)
 //
-//            val inputStream = connection.inputStream
-//            val bitmap = BitmapFactory.decodeStream(inputStream)
+//            //cuba
+//            val imageUrl3 =
+//                "https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80"
+//            val url3 = URL(imageUrl3)
+//            val connection3 = url3.openConnection() as HttpURLConnection
+//            connection3.doInput = true
+//            connection3.connect()
+//            val inputStream3 = connection3.inputStream
+//            val bitmap3 = BitmapFactory.decodeStream(inputStream3)
 //
-//            // Switching scope to Main Thread to access the View
-//            launch(Dispatchers.Main) {
-//        val imageView4 = findViewById<ImageView>(R.id.imageView4)
-//                imageView4.setImageBitmap(bitmap)
-//            }
-//        }
+//            //sampler
+//            val imageUrl4 =
+//                "https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80"
+//            val url4 = URL(imageUrl4)
+//            val connection4 = url4.openConnection() as HttpURLConnection
+//            connection4.doInput = true
+//            connection4.connect()
+//            val inputStream4 = connection4.inputStream
+//            val bitmap4 = BitmapFactory.decodeStream(inputStream4)
+//
+//            //star
+//            val imageUrl5 =
+//                "https://images.unsplash.com/photo-1554254648-2d58a1bc3fd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2342&q=80"
+//            val url5 = URL(imageUrl4)
+//            val connection5 = url5.openConnection() as HttpURLConnection
+//            connection5.doInput = true
+//            connection5.connect()
+//            val inputStream5 = connection4.inputStream
+//            val bitmap5 = BitmapFactory.decodeStream(inputStream5)
+
+            // Switching scope to Main Thread to access the View
+            launch(Dispatchers.Main) {
+                textViewStatus.text="Image Fetch Status: Success"
+                val imageView4 = findViewById<ImageView>(R.id.imageView4) // bahamas
+                imageView4.setImageBitmap(bitmap)
+
+//                val imageView3 = findViewById<ImageView>(R.id.imageView3) //caribbean
+//                imageView3.setImageBitmap(bitmap2)
+//
+//                val imageView6 = findViewById<ImageView>(R.id.imageView6) //cuba
+//                imageView6.setImageBitmap(bitmap3)
+//
+//                val imageView5 = findViewById<ImageView>(R.id.imageView5) //sampler
+//                imageView5.setImageBitmap(bitmap4)
+//
+//                val imageView7 = findViewById<ImageView>(R.id.imageView7) //star
+//                imageView7.setImageBitmap(bitmap5)
+
+            }
+        }
         // END of buggy code block
 
     }
