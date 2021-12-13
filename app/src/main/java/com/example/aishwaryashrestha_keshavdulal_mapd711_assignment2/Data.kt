ebd99f2b4074fc0c6b213c0da2e8ebea648200e6
Aishwarya Shrestha <aishwaryashrestha6094@gmail.com>
2021-12-12 20:28:29 -0500

    Firebase successfully connected

---
+++ app/src/main/java/com/example/aishwaryashrestha_keshavdulal_mapd711_assignment2/Data.kt
@@ -0,0 +1,3 @@
+package com.example.aishwaryashrestha_keshavdulal_mapd711_assignment2
+
+class Data(val id: String?, val username: String,  val lastname: String,  val firstname: String, val city: String, val address: String, val telephone: String, val postalcode: String )
--- app/src/main/java/com/example/aishwaryashrestha_keshavdulal_mapd711_assignment2/Register.kt
+++ app/src/main/java/com/example/aishwaryashrestha_keshavdulal_mapd711_assignment2/Register.kt
@@ -11,8 +11,20 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import com.example.aishwaryashrestha_keshavdulal_mapd711_assignment2.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
 
 class Register : AppCompatActivity() {
+
+    lateinit var uname: EditText
+    lateinit var fname: EditText
+    lateinit var lname: EditText
+    lateinit var cname: EditText
+    lateinit var aname: EditText
+    lateinit var pname: EditText
+    lateinit var tname: EditText
+
+
 
     //ViewBinding
     private lateinit var binding: ActivityRegisterBinding
@@ -50,14 +62,86 @@ class Register : AppCompatActivity() {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance()
 
+        uname = findViewById(R.id.inputUsername)
+        fname = findViewById(R.id.inputFirstName)
+        lname = findViewById(R.id.inputLastName)
+        aname = findViewById(R.id.inputAddress)
+        pname = findViewById(R.id.inputPostalCode)
+        cname = findViewById(R.id.inputCity)
+        tname = findViewById(R.id.inputTelephone)
+
+
         //handle click, begin signup
         binding.buttonRegister.setOnClickListener {
             //validate data
             validateData()
+            saveUserData()
+
+        }
+    }
+
+    private fun saveUserData() {
+        val username = uname.text.toString().trim()
+        val firstname = fname.text.toString().trim()
+        val lastname = lname.text.toString().trim()
+        val city = cname.text.toString().trim()
+        val address = aname.text.toString().trim()
+        val postalcode = pname.text.toString().trim()
+        val telephone = tname.text.toString().trim()
+
+
+
+
+        if(username.isEmpty()) {
+            uname.error = "Please enter your username."
+            return
         }
 
-        var emailEt: EditText = findViewById(R.id.emailEt)
-        var passwordEt: EditText = findViewById(R.id.passwordEt)
+        if(firstname.isEmpty()) {
+            fname.error = "Please enter your firstname."
+            return
+        }
+
+        if(lastname.isEmpty()) {
+            lname.error = "Please enter your lastname."
+            return
+        }
+
+
+
+        if(city.isEmpty()) {
+            cname.error = "Please enter your city."
+            return
+        }
+
+        if(address.isEmpty()) {
+            aname.error = "Please enter your address."
+            return
+        }
+        if(postalcode.isEmpty()) {
+            pname.error = "Please enter your postal code."
+            return
+        }
+
+        if(telephone.isEmpty()) {
+            tname.error = "Please enter your contact."
+            return
+        }
+
+
+
+
+
+
+        val ref = FirebaseDatabase.getInstance().getReference("data")
+
+        val dataId = ref.push().key
+
+        val data = Data(dataId, username, firstname, lastname, city, address, postalcode, telephone  )
+
+        ref.child(dataId.toString()).setValue(data).addOnCompleteListener {
+            Toast.makeText(applicationContext, "data saved successfully", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun validateData() {
@@ -99,7 +183,7 @@ class Register : AppCompatActivity() {
                 //get current user
                 val firebaseUser = firebaseAuth.currentUser
                 val email = firebaseUser!!.email
-                Toast.makeText(this, "Acocunt created with email $email", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Account created with email $email", Toast.LENGTH_SHORT).show()
 
 
                 //open profile
